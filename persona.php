5419f70af14ea498427afffc92ebc5d20de37012
Massimo Minighin <info@studiominighin.com>
2013-01-15 06:06:24 -0800

    Create persona.php

---
+++ persona.php
@@ -0,0 +1,13 @@
+<?php
+  class Persona
+	{
+		// Proprietà come variabili con attributo pubblico
+		private $nome;
+		public $cognome;
+		public $eta;
+		// Costanti
+    	const ALTEZZA = '1.70';
+
+  		// Metodi come function
+	}
+?>

f3342c3ead71c0eea97703a9d247a91aee8fb022
Massimo Minighin <info@studiominighin.com>
2013-01-15 06:05:16 -0800

    Create dipendente.php

---
+++ dipendente.php
@@ -0,0 +1,12 @@
+<?php
+  class Dipendente
+	{
+		// Proprietà come variabili con attributo pubblico
+			
+			public $nome;
+			public $cognome;
+			public $eta;
+			protected $turni;
+			public $mansione;
+	}
+?>
